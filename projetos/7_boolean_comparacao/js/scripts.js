b5a127c7905d5f5e6ec868203d22d30383fd638d
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-05 01:17:34 -0300

    boolean_comparacao

---
+++ projetos/7_boolean_comparacao/js/scripts.js
@@ -0,0 +1,14 @@
+console.log(1 > 2);
+console.log(5 < 10);
+console.log(3 >= 3);
+console.log(5 <= 4);
+
+console.log(5 == 4);
+console.log('Brendo' != 'Brendo');
+console.log(3 === '3');
+console.log(4 === 4);
+
+console.log(5 == 5)
+console.log('xxt' != 'xt');
+console.log(3 === 3);
+console.log(4 ===5);

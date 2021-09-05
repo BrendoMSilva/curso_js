595585704826d9532f30148be30427e3da59c039
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-05 01:52:12 -0300

    op_logico

---
+++ projetos/8_boolean_op_logico/js/scripts.js
@@ -0,0 +1,21 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+
+console.log(3 == 3 && 'Brendo' == 'Brendo');
+console.log('Silva' == 'Mota' || false);
+
+console.log(!(!(true && true)));
+
+console.log(true && true);
+console.log(false || false);
+
+console.log(4 < 2 && 3 == 5);
+console.log(4 < 2 || 3 == 3);
+
+console.log(4 == 4 && 'xxt' == 'Xxt');
+console.log('brendinho' == 'brendinho' || true);
+
+console.log(!(!(false && false)));
+
+console.log(true && false);
+console.log(false || true)

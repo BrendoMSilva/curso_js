7617e2729a74673c1703701d663fb0a2b059fbc3
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-06 21:03:59 -0300

    Update scripts.js

--- projetos/9_boolean_op_ternario/js/scripts.js
+++ projetos/9_boolean_op_ternario/js/scripts.js
@@ -1,3 +1,7 @@
 console.log(5 > 2 ? 'É sim' : 'Não é');
 console.log(false ? 5 : 4 );
 console.log('Brendo' == 'Brendo' ? 'Ola brendo' : 'você não é o brendo');
+
+console.log(2 <= 3 ? '2 é menor ou igual á 3' : '2 não é menor ou igual a 3');
+console.log(true && true ? 30 : 24);
+console.log(false || false ? 50 : 20);

165ede2028da3a9e9d424cefde6fd53379bc48c0
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-08 20:55:45 -0300

    exercicio_04

---
+++ projetos/exercicios/4_exercicio_04/js/script.js
@@ -0,0 +1,6 @@
+console.log(true && true);
+console.log(false && false);
+console.log(false || false);
+console.log(!(true && true));
+console.log(!(false || false));
+console.log(!(false && false));

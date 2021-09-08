cd5e8123a65b2ee46795192188c7fde7d0fde8a4
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-08 20:03:12 -0300

    exercicio_01

---
+++ projetos/exercicios/1_exercicio_01/js/script.js
@@ -0,0 +1,5 @@
+console.log('Primeiro exercicio usando strings');
+console.log("Usando aspas duplas");
+console.log('Meu' + 'nome' + 'é:' + 'Brendo');
+console.log(`somando 4 + 3 = ${4 +3}`);
+console.log(`esse e um template literals`);

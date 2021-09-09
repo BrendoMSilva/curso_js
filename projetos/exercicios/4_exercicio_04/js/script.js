2341244868b41613a2445d05b7a3f3a15833a647
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-08 21:01:44 -0300

    Update script.js

--- projetos/exercicios/4_exercicio_04/js/script.js
+++ projetos/exercicios/4_exercicio_04/js/script.js
@@ -1,6 +1,7 @@
-console.log(true && true);
-console.log(false && false);
-console.log(false || false);
+console.log(2 == 3 && 4 > 1);
+console.log(3 <= 4 || 'Brendo' == 'joao');
+console.log(4 == 3 || 3 < 2);
+console.log(!(2 == 2));
 console.log(!(true && true));
 console.log(!(false || false));
 console.log(!(false && false));

a63ff044ed422558d1f39cdb129123298d13cc62
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-11 18:09:27 -0300

    variaveis

---
+++ projetos_2/1_variaveis/js/scripts.js
@@ -0,0 +1,35 @@
+let nome = 'Brendo';
+
+console.log(nome);
+console.log(`Meu nome é ${nome}`);
+
+let laranja = 5;
+
+console.log(laranja * laranja);
+
+nome = 'João';
+
+console.log(nome);
+
+laranja = 74887487;
+
+console.log(laranja);
+
+laranja = 'laranja';
+
+console.log(laranja);
+
+nome = 'euro';
+
+sobrenome = 'truck';
+
+console.log(`O nome do jogo é ${nome} ${sobrenome}`);
+
+valor = 20
+
+console.log(valor *(valor + valor));
+
+
+
+
+

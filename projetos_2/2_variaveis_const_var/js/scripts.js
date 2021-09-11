fef1977aacf5a253ce5a41c3d8186eb01209cb56
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-11 18:25:57 -0300

    variaveis_const_var

---
+++ projetos_2/2_variaveis_const_var/js/scripts.js
@@ -0,0 +1,10 @@
+var nome = 'Davi';
+const ip = '127.0.0.1';
+
+console.log(nome);
+console.log(ip);
+
+ip = 12; // erro por causa da troca de valor com o const.
+
+// const = o valor não pode ser alterado
+// var = forma mais antiga, vc deve obtar pelo let ou const

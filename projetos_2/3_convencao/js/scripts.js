3447e025d0799f802f324081c219bd59f203c8aa
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-12 16:58:35 -0300

    convencao

---
+++ projetos_2/3_convencao/js/scripts.js
@@ -0,0 +1,12 @@
+let nome = 'teste';
+let $nome = 'teste2';
+let _nome = 'teste3';
+let Nome = 'teste4';
+let seuNome = 'teste5'; // camelCase o mais recomendavel
+
+
+// variaves que dão erro
+
+// let 3nome = 'teste';
+// let @nome = 'teste';
+// let !nome = 'teste'; entre outros simbolos, tirando '$' e '_'.

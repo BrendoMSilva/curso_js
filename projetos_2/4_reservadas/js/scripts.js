8dcd6592b9915c681c07dd4e58f9a14610989b74
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-12 17:08:14 -0300

    reservadas

---
+++ projetos_2/4_reservadas/js/scripts.js
@@ -0,0 +1,12 @@
+// não podemos usar palavras reservadas
+// let if = 'teste';
+// let function = 'teste';
+
+
+// mas podemos usar metade delas e modificar
+
+let functionTest = 'teste';
+let ifTest = 'teste2';
+
+console.log(functionTest);
+console.log(ifTest);

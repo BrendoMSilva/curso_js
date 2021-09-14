0dcfabf2c570a6baad910bcbd544911dd576ad42
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-13 21:24:32 -0300

    prompt

---
+++ projetos_2/5_prompt/js/scripts.js
@@ -0,0 +1,11 @@
+let idade = prompt('Qual a sua idade?');
+
+console.log(idade);
+
+let nome = prompt('Qual o seu nome');
+
+console.log(`O seu nome é ${nome}`);
+
+let dataNascimento = prompt('Qual é a sua data de nascimento');
+
+console.log(`Voce nasceu em ${dataNascimento}`);

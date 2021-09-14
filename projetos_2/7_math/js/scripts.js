44e3ecfcecb127307a8a6580ac5446643a4272b5
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-13 21:43:59 -0300

    math

---
+++ projetos_2/7_math/js/scripts.js
@@ -0,0 +1,22 @@
+// função muito ultilizada
+
+
+// pega o maior valor = max
+let maior = Math.max(6, 10, 50, 43, 3, 5);
+
+console.log(maior);
+
+// pega o menor valor = min 
+let menor = Math.min(3, 5, 60, 2, 2, 1);
+
+console.log(menor);
+
+// arredonda = round
+let arrendondar = Math.round(5.29545);
+
+console.log(arrendondar);
+
+// arredondar pra cima = ceil
+let arrendondarParaCima = Math.ceil(5.88375873);
+
+console.log(arrendondarParaCima);

587dd9615d0debcd3cc9f3b324dc246e31a6937b
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-16 21:18:55 -0300

    else_if

---
+++ projetos_2/11_else_if/js/scripts.js
@@ -0,0 +1,25 @@
+let nome = 'Brendo';
+let idade = 28;
+
+if(nome != undefined && nome == 'Joaquim') {
+    console.log('Nome está definido!');
+} else if(nome == 'Brendo' && nome.length > 5 && idade == 50) {
+    console.log('O nome é Brendo');
+} else {
+    console.log('Não é Brendo');
+}
+
+if(1 > 2) {
+    console.log('Teste');
+} else if(1 == 1) {
+    console.log('Testando')
+}
+
+let animals = 'especies';
+let quantAnimals = 50;
+
+if(animals != undefined) {
+    console.log('são animais');
+} else if(animals == 'especies') {
+    console.log('Não sao animais');
+}

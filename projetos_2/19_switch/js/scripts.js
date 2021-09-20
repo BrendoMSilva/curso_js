dbd6f829cfe9bbda2f466afc0fd3dabf4b8d00a4
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-20 20:51:23 -0300

    switch

---
+++ projetos_2/19_switch/js/scripts.js
@@ -0,0 +1,22 @@
+// ultilizavel em casos especificos se for da sua preferencia
+
+let nome = 'Brendo';
+
+switch(nome) {
+    case 'Brendo':
+        console.log('O nome é Brendo');
+        break;
+    case 'Davi':
+        console.log('O nome é Davi');
+        break;
+    default:
+        console.log('O nome não foi encontrado!');
+        break;
+}
+
+// comparação com o if
+// if(nome == 'Brendo') {
+//      console.log('O nome e Brendo);
+// }else {
+//      console.log('O nome não foi encontrado);
+// }

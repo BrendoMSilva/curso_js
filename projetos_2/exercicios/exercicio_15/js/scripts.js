f8fd509caf857ed8e317935a48afddfc958790a1
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-21 20:33:17 -0300

    Update scripts.js

--- projetos_2/exercicios/exercicio_15/js/scripts.js
+++ projetos_2/exercicios/exercicio_15/js/scripts.js
@@ -1,5 +1,14 @@
-for(let i = 0; i < 50; i++) {
-    if(i % 2 == 1) {
-        console.log(`${i} é impar`)
+let num = 17;
+let divisoes = 0;
+
+for(let i = 1; i <= num; i++) {
+    if(num % i == 0) {
+        divisoes++;
     }
+}
+
+if(divisoes == 2) {
+    console.log(`O numero ${num} é primo`);
+} else {
+    console.log(`O numero ${num} não é primo`);
 }

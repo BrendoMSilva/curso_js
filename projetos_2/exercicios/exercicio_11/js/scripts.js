95c51a3f34863def84a68f6dda7b54c46096a67f
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-20 23:32:42 -0300

    Update scripts.js

--- projetos_2/exercicios/exercicio_11/js/scripts.js
+++ projetos_2/exercicios/exercicio_11/js/scripts.js
@@ -2,9 +2,9 @@ let cnh = true;
 let idade = 20;
 
 if(idade >= 18 && cnh == false) {
-    console.log(`Você tem ${idade} anos, mas não possui CNH.`);
+    console.log(`Você tem ${idade} anos, mas não possui CNH, não pode dirigir.`);
 } else if(idade >= 18 && cnh == true){
-    console.log(`Voce tem ${idade} anos e CNH, parabens`);
+    console.log(`Voce tem ${idade} anos e CNH, pode dirigir`);
 } else {
-    console.log('Voce não tem a idade minina e CNH');
+    console.log('Voce não tem a idade minina e CNH, para poder dirigir');
 }

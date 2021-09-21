b6aed5bba938957c7684d5a0714a54b77312f426
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-20 23:01:36 -0300

    ex_10

---
+++ projetos_2/exercicios/exercicio_10/js/scripts.js
@@ -0,0 +1,7 @@
+let velocidade = 70;
+
+if(velocidade <= 80) {
+    console.log('Você esta na velocidade permitida, continue assim!');
+}else {
+    console.log('Você foi multado por esta acima da velocidade permitida!');
+}

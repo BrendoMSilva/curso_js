0e9db19db42dca4620f00a22f7d8739d36cf069d
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-20 21:41:51 -0300

    ex_07

---
+++ projetos_2/exercicios/exercicio_07/js/scripts.js
@@ -0,0 +1,7 @@
+let maiorDeIdade = 20
+
+if(maiorDeIdade >= 18) {
+    console.log('Você é maior de idade, seja bem-vindo em nossa festa!.');
+} else {
+    console.log('Você e menor de idade, não pode entrar!');
+}

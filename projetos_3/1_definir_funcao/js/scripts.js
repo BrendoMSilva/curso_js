712f1899999a2cd9e40802211f587c0609236f90
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-22 20:02:45 -0300

    definir_funcao

---
+++ projetos_3/1_definir_funcao/js/scripts.js
@@ -0,0 +1,22 @@
+function imprimirNoConsole() {
+    console.log('ola, mundo');
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num) {
+    console.log('O numero é: ' + num);
+} 
+
+imprimirUmNumero(34);
+imprimirUmNumero(3);
+imprimirUmNumero(34);
+imprimirUmNumero(55);
+
+const numeroAleatorio = function() {
+    console.log(Math.random());
+}
+
+numeroAleatorio();
+numeroAleatorio();
+

0abdaf7364473ee679412e16c4a7eb8e0309d4ec
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-23 21:37:33 -0300

    escopo_aninhado

---
+++ projetos_3/5_escopo_aninhado/js/scripts.js
@@ -0,0 +1,40 @@
+let a = 10;
+
+function multiplicar(x, y) {
+    let a = y * x;
+
+    if(a > 10) {
+        let a = 0;
+
+        a++;
+
+        console.log(a);
+    }
+
+    console.log(a);
+
+}
+
+console.log(a);
+
+multiplicar(3, 7);
+
+///////////////////////////////
+
+let s = 20;
+
+function soma(x, y) {
+    let s = x + y;
+    
+    if(s > 10) {
+        let s = 0;
+        console.log(s)
+    }
+
+    console.log(s);
+
+}
+
+console.log(s);
+
+soma(10, 30);

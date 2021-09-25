f55efe93d6e0fb1ec8e26334edd9e0ece9bcf90e
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-24 21:41:49 -0300

    arrow_function_2

---
+++ projetos_3/7_arrow_function 2/js/scripts.js
@@ -0,0 +1,9 @@
+const multiplicarPorDois = (x) => {
+    return x * 2;
+}
+
+console.log(multiplicarPorDois(2));
+
+const multiplicarPorTres = x => x * 3; // aconselhavel  usar apenas quando não tiver muitos parametros
+
+console.log(multiplicarPorTres(3));

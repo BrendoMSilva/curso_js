241248f05578e7242c1e4b88bda3bd7d794735f5
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-30 21:29:10 -0300

    mais_sobre_arrays

---
+++ projetos_4/2_mais sobre arrays/js/scripts.js
@@ -0,0 +1,8 @@
+let arr = [1, 3, 6, 7, 12];
+let nomes = ['Brendo', 'Desenvolvedor', 'Analista'];
+let bool = [true, false, true];
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(bool[2]);
+console.log(arr[arr.length - 1]);

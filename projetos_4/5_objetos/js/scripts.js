58053096c0ca4397a7ad0fba7b94a7d96805f782
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-30 22:36:36 -0300

    objetos

---
+++ projetos_4/5_objetos/js/scripts.js
@@ -0,0 +1,18 @@
+let carro = {
+    roda: 4,
+    marca: 'volkswagen',
+    modelo: 'Jetta',
+    ano: 2020,
+
+    barulho: function() {
+        console.log('vrum vrummm')
+    }
+};
+
+console.log(carro.marca);
+console.log(carro.modelo);
+console.log(carro.ano);
+carro.barulho();
+
+// [] = array
+// {} = objeto

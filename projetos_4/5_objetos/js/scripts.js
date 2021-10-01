8a0e07f330f0dd4910bfcb533c158a1123ad522f
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-09-30 22:44:24 -0300

    Update scripts.js

--- projetos_4/5_objetos/js/scripts.js
+++ projetos_4/5_objetos/js/scripts.js
@@ -14,5 +14,39 @@ console.log(carro.modelo);
 console.log(carro.ano);
 carro.barulho();
 
+let concorencia = {
+    roda: 4,
+    marca: 'chevrolet',
+    modelo: 'onix',
+    ano: 2021
+};
+
+console.log(concorencia.marca);
+console.log(concorencia.modelo);
+console.log(concorencia.ano);
+
+let concorenciaDois = {
+    roda: 4,
+    marca: 'honda',
+    modelo: 'honda civic',
+    ano: 2020
+};
+
+console.log(concorenciaDois.marca);
+console.log(concorenciaDois.modelo);
+console.log(concorenciaDois.ano);
+
+let concorenciaTres = {
+    roda: 4,
+    marca: 'toyota',
+    modelo: 'corolla',
+    ano:2019
+}
+
+console.log(concorenciaTres.marca);
+console.log(concorenciaTres.modelo);
+console.log(concorenciaTres.ano);
+
+
 // [] = array
 // {} = objeto

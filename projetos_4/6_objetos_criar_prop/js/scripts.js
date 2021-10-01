d38d449989cf4304bba6627e714583507f6af9dc
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-01 20:25:32 -0300

    Update scripts.js

--- projetos_4/6_objetos_criar_prop/js/scripts.js
+++ projetos_4/6_objetos_criar_prop/js/scripts.js
@@ -17,3 +17,21 @@ pessoa.casado = false;
 console.log(pessoa.casado);
 
 console.log(pessoa);
+
+let carro = {
+    marca: 'Ford',
+    modelo: 'Fusion',
+    ano: 2018
+}
+
+console.log(carro);
+
+carro.motor ='2.0';
+carro.rodas = 4;
+carro.airBag = 'incluido';
+carro.transmissao = '6F35'
+carro.cor = 'preto';
+console.log(carro);
+
+delete carro.roda;
+console.log(carro.roda);

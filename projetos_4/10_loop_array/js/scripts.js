b8d57dc28042146b09be9bc531789ce55d440659
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-01 21:08:53 -0300

    Update scripts.js

--- projetos_4/10_loop_array/js/scripts.js
+++ projetos_4/10_loop_array/js/scripts.js
@@ -3,3 +3,15 @@ let nomes = ['Pedro', 'Maria', 'Brendo'];
 for(let i = 0; i <= nomes.length; i++) {
     console.log(nomes[i]);
 }
+
+let marcas = ['Nike', 'Adidas', 'Puma'];
+
+for(let seguencia = 0; seguencia <= marcas.length; seguencia++) {
+    console.log(marcas[seguencia]);
+}
+
+let numeros = [1, 4, 34, 66, 50];
+
+for(let num = 0; num <= numeros.length; num++) {
+    console.log(numeros[num]);
+}

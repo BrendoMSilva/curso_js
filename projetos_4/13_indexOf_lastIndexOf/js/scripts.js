f88fa6627aec8be020a1098e2caedad145b2e728
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-05 21:43:22 -0300

    indexOf_lastIndexOf

---
+++ projetos_4/13_indexOf_lastIndexOf/js/scripts.js
@@ -0,0 +1,7 @@
+let num = [5, 6, 2, 5, 9, 6, 2];
+
+console.log(num.indexOf(2));
+console.log(num.lastIndexOf(2))
+
+// indexof: encontra o indice de um determinado elemento.
+// lastIndexOf: encontra o ultimo indice de um elemento.

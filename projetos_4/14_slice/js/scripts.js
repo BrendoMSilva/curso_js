ea4aa014276348ed5896b10c754b50dfadd9156b
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-06 21:16:26 -0300

    slice

---
+++ projetos_4/14_slice/js/scripts.js
@@ -0,0 +1,12 @@
+let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(num.slice(4,5));
+console.log(num.slice(4, 6));
+
+console.log(num.slice(2));
+
+console.log(num.slice(-2));
+console.log(num.slice(3, -2));
+
+// retorna um array a partir de outro array
+// o array retornado é determinado pelos parametros que sao os indices de inicio e fim do novo array 

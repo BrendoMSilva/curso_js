2280cfc69f9d0af6454c022c078f6ada2dc8fe26
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-07 20:09:14 -0300

    Update scripts.js

--- projetos_4/18_trim/js/scripts.js
+++ projetos_4/18_trim/js/scripts.js
@@ -2,4 +2,12 @@ let texto = '  \n  teste  \n ';
 
 console.log(texto.trim()); // teste
 
+let nome = '  \n Brendo  \n ';
+
+// ola    Brendo, tudo bem?
+
+let msgcorrigida = nome.trim();
+
+console.log(nome);
+console.log(msgcorrigida);
 // remove tudo que não é string

4eeb20ece5ab65fcacb96c3e87c2e6a8707018b4
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-07 20:49:26 -0300

    Update scripts.js

--- projetos_4/19_padStart/js/scripts.js
+++ projetos_4/19_padStart/js/scripts.js
@@ -5,4 +5,8 @@ console.log(milAoContrario.padStart(4, '0'));
 let sku = '34';
 
 console.log(sku.padStart(6, '0'));
+
+let sku2 = '8949';
+
+console.log(sku2.padStart(6, '0'));
 // inserir caracteres antes da string

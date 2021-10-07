79b6a8fee4d54f77c368a8e36368efc95a442da3
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-07 20:57:31 -0300

    Update scripts.js

--- projetos_4/20_split/js/scripts.js
+++ projetos_4/20_split/js/scripts.js
@@ -4,4 +4,8 @@ let palavras = frase.split(' ');
 
 console.log(palavras);
 
+let produtos = 'banana;maçã;uva;melancia;jaca;melão';
+
+console.log(produtos.split(';'));
+
 // divide uma string por um separador, e retorna um array

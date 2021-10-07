87433957d962590dcabe39a029ce971a29be833d
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-06 21:51:27 -0300

    Update scripts.js

--- projetos_4/17_reverse/js/scripts.js
+++ projetos_4/17_reverse/js/scripts.js
@@ -1,5 +1,10 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log(arr.reverse());
+console.log(num.reverse());
+
+let nome = ['BMW', 'Audi', 'Ferrari', 'VW', 'Ford'];
+
+console.log(nome.reverse());
+
 
 // inverte um array

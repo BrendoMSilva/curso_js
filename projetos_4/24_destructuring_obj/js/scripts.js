fe4aca997b0f335bb74513a3e271b9eb32e835a2
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-08 20:58:58 -0300

    Update scripts.js

--- projetos_4/24_destructuring_obj/js/scripts.js
+++ projetos_4/24_destructuring_obj/js/scripts.js
@@ -8,4 +8,20 @@ const {name: fname, lastname: lname} = person;
 console.log(fname);
 console.log(lname);
 
+
+let obj = {
+    marca: 'Ferrari',
+    cor: 'Red',
+    portas: 2,
+    speed: true
+}
+
+const {marca: vMarca, cor: vCor, portas: vPortas, speed: vSpeed} = obj;
+
+console.log(vMarca);
+console.log(vCor);
+console.log(vPortas);
+console.log(vSpeed);
+
+
 // podemos definir variaveis com propriedades do objeto com uma notação diferente, chamada destructuring

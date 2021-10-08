550516fba8f2947a6b2d1bfe9d8f0a308ad92ce6
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-08 20:22:38 -0300

    join

---
+++ projetos_4/21_join/js/scripts.js
@@ -0,0 +1,11 @@
+let frase = 'Testando o metodo join';
+
+let palavras = frase.split(' ');
+
+let novaFrase = palavras.join('@');
+
+console.log(novaFrase);
+
+console.log(palavras.join(' '));
+
+// junta elementos em uma array em uma frase, por meio de um separador

5a70c34ee54c96d4eeed7ce463122340b673704a
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-08 21:32:03 -0300

    destructuring_array

---
+++ projetos_4/25_destructuring_array/js/scripts.js
@@ -0,0 +1,10 @@
+let nome = ['Brendo', 'Neymar', 'CR7', 'Messi'];
+
+let [nomeA, nomeB, nomeC, nomeD] = nome;
+
+console.log(nomeA);
+console.log(nomeB);
+console.log(nomeC);
+console.log(nomeD);
+
+// podemos definir variaveis com os valores de um array utilizando o destructuring

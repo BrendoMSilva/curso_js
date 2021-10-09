36c2308e4180a21d9f21ffa93591075f6512808d
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-08 22:43:11 -0300

    mais_sobre_JSON

--- projetos_4/26_JSON/js/scripts.js
+++ projetos_4/26_JSON/js/scripts.js
@@ -1,11 +1,4 @@
-let pessoa = {
-    "nome": "Brendo",
-    "idade": 20
-}
 
-console.log(pessoa.nome);
-console.log(pessoa.idade);
-
-// JSON = JavaScript Object Notation;
-// ultilizado para comunicação entre serviços, ex: back-end <-> front-end
-// basicamente um tipo de dado padronizado, que lembra muito os objetos do javascropt;
+// rigoroso na estrutura, ou seja, voce deve seguir o padrao imposto por este formato;
+// apenas aspas duplas;
+// nao aceita comentarios;

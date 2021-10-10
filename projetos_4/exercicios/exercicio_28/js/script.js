01120ac78af38ba44b4e0af2018faed9f8de8097
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-10 14:52:14 -0300

    ex_28

---
+++ projetos_4/exercicios/exercicio_28/js/script.js
@@ -0,0 +1,10 @@
+let onibus = {
+    Rodas: 8,
+    limitePassageiros: 40,
+    Portas: 2
+};
+
+console.log(onibus);
+console.log(onibus.Rodas);
+console.log(onibus.limitePassageiros);
+console.log(onibus.Portas);

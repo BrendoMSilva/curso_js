b536b2fa1b8900ceb3311b9cb458c53b738d404a
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-10 15:06:41 -0300

    ex_29

---
+++ projetos_4/exercicios/exercicio_29 copy/js/script.js
@@ -0,0 +1,13 @@
+let onibus = {
+    Rodas: 8,
+    limitePassageiro: 40,
+    Porta: 2
+}
+
+onibus.janela = 20;
+console.log(onibus.janela);
+console.log(onibus)
+
+delete onibus.Rodas
+console.log(onibus.Rodas);
+console.log(onibus);

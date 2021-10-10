95b7c061f364cf49706d4a5c6f92cb60ad5eea29
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-10 15:15:58 -0300

    ex_30

---
+++ projetos_4/exercicios/exercicio_30/js/script.js
@@ -0,0 +1,7 @@
+let nomes = ['Brendo', 'Neymar', 'CR7', 'Messi', 'Antony'];
+
+if(nomes.includes('Brendo')) {
+    console.log('Nome encontrado!');
+} else {
+    console.log('Nome nao existente');
+}

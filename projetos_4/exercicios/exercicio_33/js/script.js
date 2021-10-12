037635e5333c2f1fb7236a78bf82e2202bd74888
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-11 21:49:40 -0300

    ex_33

---
+++ projetos_4/exercicios/exercicio_33/js/script.js
@@ -0,0 +1,12 @@
+let time = {
+    "time": "SPFC",
+    "age": 91,
+    "idolo": "Rogerio Ceni"
+}
+
+console.log(time);
+console.log(time.time);
+console.log(time.age);
+console.log(time.idolo);
+
+

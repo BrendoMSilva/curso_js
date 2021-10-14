3c148a68197193710d5eda0800cd425f1e3c1a11
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-13 21:56:27 -0300

    metodos

---
+++ projetos_5/1_metodos/js/script.js
@@ -0,0 +1,15 @@
+let cachorro = {
+    latir: function() {
+        console.log('Au Au');
+    },
+    rosnar: function() {
+        console.log('Grrrrrrrrrr');
+    }
+}
+
+cachorro.latir();
+cachorro.rosnar();
+
+// Propriedadess que servem como funcoes
+// Ou seja, as acoes dos objetos
+// Invocamos os metodos da mesma maneira que as funcoes

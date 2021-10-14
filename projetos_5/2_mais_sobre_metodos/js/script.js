d2f1800338a62601ea9fed650548cd664d1d2821
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-13 22:17:02 -0300

    Update script.js

--- projetos_5/2_mais_sobre_metodos/js/script.js
+++ projetos_5/2_mais_sobre_metodos/js/script.js
@@ -11,5 +11,29 @@ let pessoa = {
 pessoa.setNome('Brendo');
 console.log(pessoa.getNome());
 
+
+let cachorro = {
+    raca: 'SRD',
+    latir: function() {
+        console.log('Au Au');
+    },
+    rosnar: function() {
+        console.log('Grrrrrrrrrr');
+    },
+    setRaca: function(raca) {
+        this.raca = raca;
+    },
+    getRaca: function() {
+        return 'A raça é ' + this.raca;
+    }
+}
+
+console.log(cachorro.raca);
+cachorro.setRaca('Pastor Alemão');
+console.log(cachorro.raca);
+console.log(cachorro.getRaca());
+
+
+
 // normalmente os metodos interagem com os objetos;
 // ate mudando os valores das suas propriedades para corresponder a logica do programa desenvolvido

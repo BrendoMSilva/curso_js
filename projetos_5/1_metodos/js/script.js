9d010cc5dcd2bd91b96ea51c5cfe8ee8bb0115aa
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-13 22:00:48 -0300

    Update script.js

--- projetos_5/1_metodos/js/script.js
+++ projetos_5/1_metodos/js/script.js
@@ -10,6 +10,18 @@ let cachorro = {
 cachorro.latir();
 cachorro.rosnar();
 
+let pessoa = {
+    pergunta: function() {
+        console.log('ola, tudo bem?');
+    },
+    resposta: function() {
+        console.log('ola, tudo bem. obrigado por perguntar');
+    }
+}
+
+pessoa.pergunta();
+pessoa.resposta();
+
 // Propriedadess que servem como funcoes
 // Ou seja, as acoes dos objetos
 // Invocamos os metodos da mesma maneira que as funcoes

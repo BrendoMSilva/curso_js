a24177e386c2ea12c783f480b3af7c1bbbbdef8d
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-15 21:54:18 -0300

    Update script.js

--- projetos_5/8_classes_construtor_com_metodos/js/script.js
+++ projetos_5/8_classes_construtor_com_metodos/js/script.js
@@ -10,12 +10,22 @@ Cachorro.prototype.uivar = function() {
     console.log('Aaaauuuuuuuuuuu');
 }
 
+Cachorro.prototype.latir = function() {
+    console.log('au au');
+}
+
+Cachorro.prototype.correr = function() {
+    console.log('o cachorro estar correndo');
+}
+
 let pug = new Cachorro('pug', 4, 'preto', 'preto', 'kevin');
 
 
 console.log(pug.nome);
 console.log(pug);
 pug.uivar();
+pug.latir();
+pug.correr();
 
 // alem de propriedades, podemos criar a classe base ja com metodos;
 // basta definir ao prototype o metodo desejado;

df75c68fb4876e87db32de09b3cace8bc03ad2fd
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-15 21:30:03 -0300

    classe_instancia_new

---
+++ projetos_5/7_classes_construtor_por_new/js/script.js
@@ -0,0 +1,9 @@
+function Cachorro(raca) {
+    this.raca = raca;
+}
+
+let husky = new Cachorro('HUSKY');
+
+console.log(husky.raca);
+
+// em muitas linguagens temos a possibilidade de instanciar um objeto com new, no JS tambem;

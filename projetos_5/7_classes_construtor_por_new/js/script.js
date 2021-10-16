9d6aa3e0a5fe1fe7ce4b283c2d041ec712868887
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-15 21:37:37 -0300

    Update script.js

--- projetos_5/7_classes_construtor_por_new/js/script.js
+++ projetos_5/7_classes_construtor_por_new/js/script.js
@@ -1,9 +1,17 @@
-function Cachorro(raca) {
+function Cachorro(raca, patas, pelos, olhos) {
     this.raca = raca;
+    this.patas = patas;
+    this.pelos = pelos;
+    this.olhos = olhos;
+    this.nome = function() {
+        console.log(' O nome do cachorro é BOB');
+    }
 }
 
-let husky = new Cachorro('HUSKY');
+let husky = new Cachorro('HUSKY', 4, 'Branco', 'Azul');
 
+console.log(husky);
 console.log(husky.raca);
+husky.nome();
 
 // em muitas linguagens temos a possibilidade de instanciar um objeto com new, no JS tambem;

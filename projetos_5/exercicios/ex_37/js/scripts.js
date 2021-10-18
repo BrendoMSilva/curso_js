9d916f7ea321ae0fda53fa7a87bd29344b17ffc9
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-17 21:11:21 -0300

    ex_37

--- projetos_5/exercicios/ex_37/js/scripts.js
+++ projetos_5/exercicios/ex_37/js/scripts.js
@@ -1,14 +1,79 @@
-class Banco {
-    constructor(saldo, saque, deposito) {
-        this.saldo = saldo;
-        this.saque = saque;
-        this.deposito = deposito;
+class Ecommerce {
+
+    constructor(itens, qtd, valorTotal) {
+        this.itens = itens;
+        this.qtd = qtd;
+        this.valorTotal = valorTotal;
+    }
+
+
+    addItem(item) {
+
+        let contador = 0;
+
+        for(let itemCarrinho in this.itens) {
+            if(this.itens[itemCarrinho].id == item.id) {
+                this.itens[itemCarrinho].qtd += item.qtd;
+            }
+        }
+
+        if(contador == 0) {
+            this.itens.push(item);
+        }
+
+        this.qtd += item.qtd;
+        this.valorTotal += item.preco * item.qtd;
+    }
+
+    removeItem(item) {
+        
+        for(let itemCarrinho in this.itens) {
+            if(this.itens[itemCarrinho].id == item.id) {
+
+                let obj = this.itens[itemCarrinho];
+                let index = this.itens.findIndex(function(obj) {return obj.id == item.id});
+
+                this.qtd -= this.itens[itemCarrinho].qtd;
+                this.valorTotal -= this.itens[itemCarrinho].preco * this.itens[itemCarrinho].qtd;
+
+                this.itens.splice(index, 1);
+
+
+                
+            }
+        }
     }
 }
+    
 
-let banco = new Banco('R$ 200,00', 'R$ 200,00', 'R$ 150,00');
 
-console.log(banco);
-console.log(banco.saldo);
-console.log(banco.saque);
-console.log(banco.deposito);
+
+let carrinho = new Ecommerce([
+    {
+        id: 01,
+        nome:'Celular',
+        qtd: 1,
+        preco: 2000,
+        
+    },
+    {
+        id: 02,
+        nome:'Blusa',
+        qtd: 3,
+        preco: 230
+    }
+], 4, 2690);
+
+console.log(carrinho);
+
+carrinho.addItem({id: 01, nome: "Camisas", qtd: 2, preco: 30});
+
+console.log(carrinho);
+
+carrinho.addItem({id: 03, nome: "Boné", qtd: 1, preco: 10});
+
+console.log(carrinho);
+
+carrinho.removeItem({id: 02, nome: "Blusa", qtd: 3, preco: 230});
+
+console.log(carrinho);

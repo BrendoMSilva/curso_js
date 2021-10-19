7f9dfa3ae8047343772b7a59c375f7088dc5f295
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-18 21:09:10 -0300

    ex_39

---
+++ projetos_5/exercicios/ex_39/js/scripts.js
@@ -0,0 +1,30 @@
+class Carro {
+    constructor(marca, cor, gasolinaRestante, consumo) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gasolinaRestante = gasolinaRestante;
+        this.consumo = consumo;
+    }
+
+    dirigir(km) {
+
+        let litroConsumidos = km / this.consumo;
+        this.gasolinaRestante -= litroConsumidos;
+    }
+
+    abastecer(litro) {
+        this.gasolinaRestante += litro;
+    }
+
+}
+
+
+let carro = new Carro('Ferrari', 'Red', 100, 5);
+
+console.log(carro);
+
+carro.dirigir(100);
+console.log(carro);
+
+carro.abastecer(10);
+console.log(carro);

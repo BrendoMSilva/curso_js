b2810cd32e43528ae083aef9240508b2b1c9a635
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-20 19:12:49 -0300

    strict

---
+++ projetos_6/2_Strict_mode/js/script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+let opa = 'Teste';
+
+// delete Object.prototype;
+
+function teste() {
+    "use strict"
+   let testando = 'teste';
+}
+
+teste();
+
+// false.prop = '';
+// 'teste'.prop = '';
+
+// Deixa o javascript mais rigoroso na hora de se programar;
+// Deve ser declarado no topo de arquivos ou funções;
+// Colocar o strict ajuda você a codificar de forma correta e não vai impedir/limitar nadad no seu software ou programa;

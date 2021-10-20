fb0b75081ec1a16b486e3d8f05cb6e8468b829a3
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-20 19:59:43 -0300

    debugger

---
+++ projetos_6/4_debugger/js/script.js
@@ -0,0 +1,18 @@
+let a = 2;
+let b = 5;
+
+if(a == b) {
+    a = b + 2;
+}
+
+for(let i = 3; i > 10; i++) {
+    a = b * 2; 
+}
+
+debugger;
+
+if(a < 10) {
+    a == 25;
+}
+
+// funcionalidade que para o código quando atingir a linha debugger;

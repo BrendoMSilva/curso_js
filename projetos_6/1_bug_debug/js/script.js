ad6f079c7d6e2f88fe0604ffc358fef6797cf38d
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-20 19:12:36 -0300

    bug_debug

---
+++ projetos_6/1_bug_debug/js/script.js
@@ -0,0 +1,8 @@
+/*
+Bug: Problema que ocorre no código, muitas vezes por erro do programador, que impede o funcionamento correto do mesmo;
+
+Debug: O ato de resolver os bugs encontrados no código ou também a forma que é feita a 
+visualização de valores de variáveis ou fluxo do código;
+
+
+*/ 

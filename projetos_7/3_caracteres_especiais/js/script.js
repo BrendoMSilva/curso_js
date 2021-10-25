d277d9ce711eca008db130fe43338260a4a454c6
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-25 20:54:16 -0300

    caracteres_especiais

---
+++ projetos_7/3_caracteres_especiais/js/script.js
@@ -0,0 +1,49 @@
+const pontoRegex = /./;
+
+
+
+console.log(".");
+console.log(pontoRegex.test("asd"));
+console.log(pontoRegex.test("  "));
+console.log(pontoRegex.test("123"));
+console.log(pontoRegex.test("123sad"));
+
+
+const dRegex = /\d/;  // [0-9]
+
+
+
+console.log("d");
+console.log(dRegex.test("asd"));
+console.log(dRegex.test("  "));
+console.log(dRegex.test("123"));
+console.log(dRegex.test("123sad"));
+
+
+
+const Regex2 = /\D/;  // [^0-9]
+
+console.log("\D");
+console.log(Regex2.test("asd"));
+console.log(Regex2.test("  "));
+console.log(Regex2.test("123"));
+console.log(Regex2.test("123sad"));
+
+
+const sRegex = /\s/;  
+
+console.log("\s");
+console.log(sRegex.test("asd"));
+console.log(sRegex.test("  "));
+console.log(sRegex.test("123"));
+console.log(sRegex.test("123sad"));
+
+
+
+const wRegex = /\w/;  
+
+console.log("\w");
+console.log(wRegex.test("asd"));
+console.log(wRegex.test("  "));
+console.log(wRegex.test("123"));
+console.log(wRegex.test("123sad"));

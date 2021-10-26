5a1a96bb5999e58d12fb669bdbc8531a4e6a3bfd
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-26 01:45:26 -0300

    metodo_exec

---
+++ projetos_7/9_metodo_exec/js/script.js
@@ -0,0 +1,9 @@
+let teste = /\d+/.exec('O número 100');
+
+console.log(teste);
+console.log(teste.index);
+
+const digitos = /\d+/;
+
+console.log(digitos.exec('Tem o número 100 aqui'));
+console.log(digitos.exec('Tem o número aqui'));

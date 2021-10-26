65e61dfe2a315aea955ee11a4417e2acb958df79
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-26 18:42:12 -0300

    validando_data_nascimento

--- projetos_7/14_validando_data_nascimento/js/script.js
+++ projetos_7/14_validando_data_nascimento/js/script.js
@@ -1,7 +1,6 @@
-let validandoDataDeNasc = /^[0-9]{2}[/][0-9]{2}[/][0-9]{4}/;
+let validandoDataDeNasc = /^[0-31]{2}[/][0-12]{2}[/][19020-2021]{4}/;
 
 console.log(validandoDataDeNasc.test('24/01/2010'));
 console.log(validandoDataDeNasc.test('25/12/15'));
 console.log(validandoDataDeNasc.test('2/2/2020'));
-console.log(validandoDataDeNasc.test('30/02/1999'));
-console.log(validandoDataDeNasc.test('35/10/2001')); // um erro com o dia, mas deu true;
+console.log(validandoDataDeNasc.test('35/10/2001')); 

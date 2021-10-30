d3a77efb64656daba6f36c74369e22722ea19c17
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-10-30 00:32:07 -0300

    Update script.js

--- projetos_8/promises/js/script.js
+++ projetos_8/promises/js/script.js
@@ -3,6 +3,5 @@ let promessa = Promise.resolve(4 + 8);
 console.log('Algum código');
 
 promessa.then((value) => console.log(`A soma é ${value}`));
-
 // promessa.then((value) => console.log{ return value + 2}
 // .then((value) => {console.log(value)});

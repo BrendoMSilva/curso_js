88cfc30dc341c83fe15863ef9e7cf9660955ce34
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-11-02 12:21:21 -0300

    by_query

---
+++ projetos_9/by_query/js/script.js
@@ -0,0 +1,9 @@
+console.log(document.querySelector('#container-principal h1')); // #: que dizer que é uma id
+
+console.log(document.querySelector('h1'));
+
+console.log(document.querySelector('#segundo-container'));
+
+console.log(document.querySelector('div h2'));
+
+console.log(document.querySelector('ul .item-vermelho'));

812fc7e9e75388c39b4219be257c025aeaf61262
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-11-10 18:05:00 -0300

    objeto_evento

--- projetos_10/objeto_evento/js/script.js
+++ projetos_10/objeto_evento/js/script.js
@@ -1,18 +1,12 @@
 let botao = document.querySelector('#btn');
 let botaoRemover = document.querySelector('#btn2');
-let p = document.querySelector('p');
 
 function msg(e) {
-    console.log('Clicou no button');
-    e.stopPropagation();
+    console.log(e);
 }
 
 botao.addEventListener('click', msg);
 
 botaoRemover.addEventListener('click', function(event) {
     console.log(event);
-});
-
-p.addEventListener('click', function() {
-    console.log('clicou no paragrafo');
 });

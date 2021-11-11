a48e4dc7263912044b2c8e23827ad9d0c9c5155c
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-11-10 21:43:03 -0300

    evento_por_tecla

---
+++ projetos_10/evento_por_tecla/js/script.js
@@ -0,0 +1,14 @@
+window.addEventListener('keydown', function(e) {
+
+    if(e.key == 'v') {
+        console.log('Apertou a tecla V');
+    }
+
+});
+
+window.addEventListener('keyup', function(e) {
+
+    if(e.key == 'Enter') {
+        console.log('Soltou a tecla Enter');
+    }
+});

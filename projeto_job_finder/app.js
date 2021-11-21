08813aa3f40c32d93a0e225db6180f26744d4997
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-11-21 16:10:00 -0300

    test
    teste de conexão

--- projeto_job_finder/app.js
+++ projeto_job_finder/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection')
 
 const PORT = 3000;
 
 app.listen(PORT, function() {
     console.log(`O Express está funcionando na porta ${PORT}`);
 });
+
+// db connectiopn
+db
+    .authenticate()
+    .then(() => {
+        console.log('conectou ao banco com sucesso');
+    })
+    .catch(err => {
+        console.log('ocorreu um erro ao conectar', err);
+    });
 
 app.get('/', (req, res) => {
     res.send('Esta funcionando 5');

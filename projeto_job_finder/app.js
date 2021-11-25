aa89e355a47df3691d762148e0c0ec1fac0c7c2f
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-11-24 21:53:09 -0300

    create_views

--- projeto_job_finder/app.js
+++ projeto_job_finder/app.js
@@ -1,6 +1,6 @@
 const express    = require('express');
 const exphbs     = require('express-handlebars');
-const app        = expreess();
+const app        = express();
 const path       = require('path');
 const db         = require('./db/connection');
 const bodyParser = require('body-parser');
@@ -8,32 +8,34 @@ const bodyParser = require('body-parser');
 const PORT = 3000;
 
 app.listen(PORT, function() {
-    console.log(`O Express está funcionando na porta ${PORT}`);
-});
-
-// body parser
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// handlebars
+    console.log(`O Express está rodando na porta ${PORT}`);
+  });
+  
+  // body parser
+  app.use(bodyParser.urlencoded({ extended: false }));
+  
+  // handle bars
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlbars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// db connectiopn
-db
+  // static folder
+app.use(express.static(path.join(__dirname, 'public')));
+  
+  // db connection
+  db
     .authenticate()
     .then(() => {
-        console.log('conectou ao banco com sucesso');
+      console.log("Conectou ao banco com sucesso");
     })
     .catch(err => {
-        console.log('ocorreu um erro ao conectar', err);
+      console.log("Ocorreu um erro ao conectar", err);
     });
 
 // routes
-app.get('/', (req, res) => {
-    res.send('Esta funcionando 3');
+app.get('/', (req, res) =>  {
+    res.render('index');
 });
 
 // jobs routes
 app.use('/jobs', require('./routes/jobs'));
-

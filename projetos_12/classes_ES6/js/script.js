a40a2bead29de216d01d3ed05a5fc723fd8cb1f4
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-11-29 15:40:40 -0300

    classes_ES6

---
+++ projetos_12/classes_ES6/js/script.js
@@ -0,0 +1,22 @@
+class Product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+
+    productWithDiscount(discount) {
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+const shirt = new Product('Camisa gola polo', 20)
+
+console.log(shirt.name)
+
+console.log(shirt.productWithDiscount(10))
+
+console.log(shirt.productWithDiscount(50))
+
+const tenis = new Product('tenis verde', 120)
+
+console.log(tenis.productWithDiscount(20))

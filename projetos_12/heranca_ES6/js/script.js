dd6c1c4152222a56c3c97307d2662e75891effa3
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-11-29 17:02:07 -0300

    heranca_ES6

---
+++ projetos_12/heranca_ES6/js/script.js
@@ -0,0 +1,45 @@
+class Product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+
+    productWithDiscount(discount) {
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+const shirt = new Product('Camisa gola polo', 20)
+
+console.log(shirt.name)
+
+console.log(shirt.productWithDiscount(10))
+
+console.log(shirt.productWithDiscount(50))
+
+const tenis = new Product('tenis verde', 120)
+
+console.log(tenis.productWithDiscount(20))
+
+// herança
+class productWithDiscount extends Product {
+    constructor(name, price, colors) {
+        super(name, price)
+        this.colors = colors
+    }
+
+    showColors() {
+        console.log('As cores são: ')
+        this.colors.forEach((color) => {
+            console.log(color)
+        });
+    }
+}
+
+const hat = new productWithDiscount('chapéu', 34.99, ['Azul', 'preto', 'cinza'])
+
+console.log(hat.name)
+
+console.log(hat.productWithDiscount(30))
+
+hat.showColors()
